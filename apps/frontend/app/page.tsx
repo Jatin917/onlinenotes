2f2a66827458cee5956c806bd2924b3fa85c1982
Jatin917 <jkchandel02@gmail.com>
2025-03-28 13:04:17 +0530

    frontend initialized

---
+++ apps/frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@repo/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="noting" appName="Online Notes">click me</Button>
+      Hello this is jatin
+    </div>
+  );
+}
